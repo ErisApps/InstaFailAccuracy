70ec1398bae1171539d52d0584652df2f45253bb
Maxime Cauvin <maxime.cauvin@epitech.eu>
2021-01-24 03:41:48 +0100

    fix/master: Makes the mod not failing when low energy due to the latest changes on NoFail

--- InstaFailAccuracy/Installers/InstaGameInstaller.cs
+++ InstaFailAccuracy/Installers/InstaGameInstaller.cs
@@ -6,15 +6,17 @@ namespace InstaFailAccuracy.Installers
 	internal class InstaGameInstaller : Installer<InstaGameInstaller>
 	{
 		private readonly PluginConfig _config;
+		private readonly GameplayCoreSceneSetupData _gameplayCoreSceneSetupData;
 
-		public InstaGameInstaller(PluginConfig config)
+		public InstaGameInstaller(PluginConfig config, GameplayCoreSceneSetupData gameplayCoreSceneSetupData)
 		{
 			_config = config;
+			_gameplayCoreSceneSetupData = gameplayCoreSceneSetupData;
 		}
 
 		public override void InstallBindings()
 		{
-			if (_config.EnableInstaFailAcc)
+			if (_config.EnableInstaFailAcc && !_gameplayCoreSceneSetupData.gameplayModifiers.noFailOn0Energy)
 			{
 				Container.BindInterfacesTo<InstaFailAccuracyGameController>().AsSingle().NonLazy();
 			}
